36fa66b9da29c2be4af6f1da92f7e4f8f3db8001
anupkeskar <anupkeskar@gmail.com>
2018-12-21 19:44:56 +0530

    Task-manager Files Uploaded
    Task-manager Files Uploaded

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1 className="App-title">TaskManager</h1>
+        <p className="App-intro">
+          To get started, Login and start creating PBIs, tasks. Monitor progress
+          and discuss . . .
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
